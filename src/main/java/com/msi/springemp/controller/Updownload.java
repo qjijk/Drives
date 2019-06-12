154394fa1e563948a5c75ec09436cfbcaf8c93f5
qgwFalcon <qgw1@outlook.com>
2019-06-12 21:15:00 +0800

    实现上传保存数据库

--- src/main/java/com/msi/springemp/controller/Updownload.java
+++ src/main/java/com/msi/springemp/controller/Updownload.java
@@ -1,5 +1,7 @@
 package com.msi.springemp.controller;
 
+import com.msi.springemp.dao.FileDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,24 +10,32 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
 import java.io.*;
 import java.net.URLEncoder;
+import java.util.Calendar;
+import java.util.Date;
 
-/**
- * 下载  测试
- * @author 王一土
- *
- */
+
 @Controller
 @RequestMapping("file")
 public class Updownload {
+    @Autowired
+    private FileDao fileDao;
 
     @RequestMapping(value="/upload",method=RequestMethod.POST)
     @ResponseBody
-    public String upload(MultipartFile file,HttpServletRequest request) throws IOException{
+    public String upload(MultipartFile file,HttpServletRequest request) throws Exception {
+
+
+
         String path = request.getSession().getServletContext().getRealPath("upload");
         String fileName = file.getOriginalFilename();
+        long a = Calendar.getInstance().getTimeInMillis();
+        System.out.println(a);
+        com.msi.springemp.pojo.File files = new com.msi.springemp.pojo.File(request.getSession().getAttribute("name").toString(),fileName,a) ;
+        fileDao.addFile(files);
+
         System.out.println(path);
         File dir = new File(path,fileName);
         if(!dir.exists()){
@@ -37,8 +47,7 @@ public class Updownload {
     }
 
     @RequestMapping("/down")
-    public void down(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception{
-        String useranme=(String) session.getAttribute("username");
+    public void down(HttpServletRequest request,HttpServletResponse response) throws Exception{
 
         String fileName = request.getSession().getServletContext().getRealPath("upload")+"/44444.sketch";
         InputStream bis = new BufferedInputStream(new FileInputStream(new File(fileName)));
@@ -48,13 +57,9 @@ public class Updownload {
         response.setContentType("multipart/form-data");
         BufferedOutputStream out = new BufferedOutputStream(response.getOutputStream());
         int len = 0;
-        if((len = bis.read()) != -1){
+        while((len = bis.read()) != -1){
             out.write(len);
             out.flush();
-            response.sendRedirect("success");
-        }
-        else{
-            response.sendRedirect("fail");
         }
         out.close();
     }
